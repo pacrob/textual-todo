63aff0af56b5a3f4b62fcb49ed1798982e352ed7
pacrob <pacrob@protonmail.com>
2023-07-24 09:23:18 -0600

    first commit

---
+++ textual_todo/__init__.py
@@ -0,0 +1,9 @@
+from textual_todo.todo import StopwatchApp
+
+def main():
+    StopwatchApp().run()
+    print("This is the main file.")
+
+if __name__ == "__main__":
+    print("This is the main file and name is main.")
+    main()
